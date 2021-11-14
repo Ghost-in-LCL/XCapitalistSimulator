97a89bd6d253898d38b67d37d64bd1e7a4db7d31
Polarnova <polarnova@polarnova.com>
2021-11-14 12:14:23 +0800

    1.add gaussian_distribution() ;2.normalize variable name; 3.add to do ;4.bug fix

--- main.cpp
+++ main.cpp
@@ -1,45 +1,66 @@
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
+/*to do:
+    init destructor
+    gaussian_distribution
+*/
 #include<iostream>
-int t=0;
+#include<cmath>
+
+int time;
+
 struct capitalist{
     int out;//the production of the capitalist's company
     int name;//the name of the capitalist
-    int s[3];//the number of every substances the capitalist possess;
-    int d[3];//the consume of every substance for the capitalist
-    int p[3];//the price which the capitalist make for every substance
-    int o[3];//export
-    bool flag;//live or die
+    int storage[3];//the number of every substances the capitalist possess;
+    int consumption[3];//the consume of every substance for the capitalist
+    int price[3];//the price which the capitalist make for every substance
+    int o[3];//export (?)
+    bool is_alive;//live or die
+	float sigma,consuming_desire;
+    //add a init destructor
 };
-void init(capitalist* a){
+
+int gaussian_distribution(int mu,int sigma/*,float possibility*/) {
+	//erfc is integral (Cumulative Distribution) of normal distribution
+	//using erfc function in cmath
+	//CDF[NormalDistribution[mu,sigma],x]==(1/2)*Erfc[(-x+mu)/Sqrt[2]*sigma]
+}
+
+void init(capitalist a) {
+    //discarded
+    //substitute with a destructor
+}
+
+void consume(capitalist a,int t) {
+	for(int i=1;i<=3;i++) {
+		a.storage[i]-=a.consumption[i];
+	}
+}
+
+void trade(capitalist a,int t) {
 
 }
-void consume(capitalist* a,int i){
+
+void make_price(capitalist a,int t) {
 
 }
-void trade(capitalist* a,int i){
 
-}
-void make_price(capitalist* a,int i){
-
-}
-void refreash(){
+void refresh() {
     
 }
-int main(){
+
+int main() {
     int t_max=0;
-    scanf("%d",&t_max);
+    std::cin>>t_max;
     capitalist person[3];
     person[0].name=0;
     person[1].name=1;
     person[2].name=2;
-    init(person);
+    init(person);//can't be compiled,substitute with a destructor
     for(int i=1;i<=t_max;i++){
         for(int j=0;j<=2;j++){
-            consume(person,i);
-            trade(person,i);
-            make_price(person,i);
+            consume(person[j],i);
+            trade(person[j],i);
+            make_price(person[j],i);//bug fixed
         }
         refresh();
     }
